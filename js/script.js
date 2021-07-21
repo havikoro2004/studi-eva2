0c4344a2165d78695d1f83cac6b69c58f6c52f3c
flata <havikoro2004@gmail.com>
2021-07-21 09:51:07 +0200

    Ajouter winner player script

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@
 var body = document.getElementById('body')
 var rollBtn = document.getElementById('roll')
 var holdBtn = document.getElementById('hold')
+var rollHoldDiv = document.getElementById('rollHold')
 var dImg = document.getElementById('dImg')
 var dotIcon1 = document.getElementById('dot-icon')
 var dotIcon2 = document.getElementById('dot-icon2')
@@ -10,11 +11,15 @@ var player1Titre = document.getElementById('p1-text')
 var player2Titre = document.getElementById('p2-text')
 var player1Current = document.getElementById('player1')
 var player2Current = document.getElementById('player2')
-var rollAudio = document.getElementById('roll-audio')
-var loseAudio = document.getElementById('lose-audio')
+var rollAudio = document.getElementById('roll-fx')
+var loseAudio = document.getElementById('lose-fx')
 var holdAudio = document.getElementById('hold-fx')
-var newAudio = document.getElementById('new-audio')
+var newAudio = document.getElementById('new-fx')
+var winAudio = document.getElementById('win-fx')
 var newGame =  document.getElementById('new')
+var currentScoreDiv1 = document.getElementById('current1')
+var currentScoreDiv2 = document.getElementById('current2')
+
 
 //Création d'une fonction rand() qui génére un nombre entre 1 et 6 
 function rand(min, max) {
@@ -37,11 +42,12 @@ let player1 = new Player('player1', 0, 0, false)
 let player2 = new Player('player2', 0, 0, false)
 let players = [player1, player2]
 
+
 //Définir début de partie
-let gameTour = 0
+var gameTour = 0
 
 // Variable qui présent le joueur courant
-let player;
+var player;
 
 
 // function Debounce 
@@ -128,6 +134,7 @@ holdBtn.addEventListener('click', debounce(function () {
   setTimeout(() => {
     dImg.src = 'images/default.jpg'
   }, 500);
+  player=players[0]
   let total = document.getElementById(players[0].name).childNodes[1]
   player.totalScore += player.currentScore
   total.textContent = player.totalScore
@@ -138,6 +145,26 @@ holdBtn.addEventListener('click', debounce(function () {
   players[0].tour = true
   changeBgColor()
 
+  //winner part
+  if (player.totalScore>=6) {
+      holdAudio.pause()
+      winAudio.play()
+      let winner = player
+      rollHoldDiv.style.display='none'
+      // remove chiledren 
+      document.getElementById(players[1].name).childNodes[3].textContent=''
+      if (winner==player1) {
+        currentScoreDiv1.style.background = 'center / contain no-repeat url("images/win1.jpg")'
+      } else {
+        currentScoreDiv2.style.background = 'center / contain no-repeat url("images/win2.jpg")'
+      }
+      body.style.background = 'linear-gradient(90deg, white 50%, white 50%)'
+      dotIcon2.style.color = 'white'
+      dotIcon1.style.color = 'white'
+      player2Titre.style.fontWeight = 'normal'
+      player1Titre.style.fontWeight = 'normal'
+      return;
+  }
 }, 400))
 
 // New game
@@ -147,3 +174,4 @@ newGame.addEventListener('click' , ()=>{
     location.reload() 
   }, 500);
 })
+
