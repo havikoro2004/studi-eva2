e316f8172b4c12a12fc61dac378d8ad1e59fcdcd
flata <havikoro2004@gmail.com>
2021-07-22 09:51:07 +0200

    Finition des sounds Fx et nettoyage du code

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
-//function random
-function rand(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
+// Variables utilisés pour le code
 var body = document.getElementById('body')
 var newGame = document.getElementById('new')
 var rollBtn = document.getElementById('roll')
@@ -12,13 +8,14 @@ var redDot1 = document.getElementById('dot-icon')
 var redDot2 = document.getElementById('dot-icon2')
 var playerTitre1 = document.getElementById('p1-text')
 var playerTitre2 = document.getElementById('p2-text')
-var newFx = document.getElementById('new-fx')
-var rollFx = document.getElementById('roll-fx')
-var holdFx = document.getElementById('hold-fx')
-var winFx = document.getElementById('win-fx')
-var loseFx = document.getElementById('lose-fx')
-var msg = () => { alert('La partie est terminée \rCliquer sur New Game pour faire une nouvelle partie') }
+const newFx = document.getElementById('new-fx')
+const rollFx = document.getElementById('roll-fx')
+const holdFx = document.getElementById('hold-fx')
+const winFx = document.getElementById('win-fx')
+const loseFx = document.getElementById('lose-fx')
+var msg = () => { alert('Cliquer sur New Game pour rejouer') }
 
+// Créer la classe joueurs
 class Players {
   constructor(name , currentScore , totalScore , tour ,winner){
     this.name = name
@@ -28,24 +25,26 @@ class Players {
     this.winner = false
   }
 }
-
+// Créer deux Objets Joueurs de la classe Players
 var player1 = new Players('player1', 0, 0, false,false)
 var player2 = new Players('player2', 0, 0, false,false)
 var players = [player1, player2]
 
 
-// Commencer la partie 
+// Variable qui initialise la partie 
 let gameTour = 0
 
 // définir le joueur courant
 let currentPlayer
 
+//roll event
+rollBtn.addEventListener('click', roll)
+
 // roll boutton function
 function roll() {
   setTimeout(() => {
     var nbrRand = rand(1, 7)
-    holdFx.pause()
-    rollFx.play()
+    generateFx(rollFx)
     switch (nbrRand) {
       case 1: dImg.src = 'images/1.jpg';
         break;
@@ -70,8 +69,7 @@ function roll() {
     }
 
     if (nbrRand == 1) {
-      rollFx.pause()
-      loseFx.play()
+      generateFx(loseFx)
       setTimeout(() => {
         dImg.src = 'images/default.jpg'
         currentPlayerCss()
@@ -85,19 +83,19 @@ function roll() {
     }
   }, 400);
 }
-//roll event
-rollBtn.addEventListener('click', roll)
 
+// Hold event 
+holdBtn.addEventListener('click', hold)
 //Hold fonction
 function hold (){
-  rollFx.pause()
-  holdFx.play()
+  dImg.src = 'images/default.jpg';
+  generateFx(holdFx)
   players[0].totalScore+=players[0].currentScore
   players[0].currentScore=0
   players[0].tour = false
   if (players[0].totalScore>=6) {
     holdFx.pause()
-    winFx.play()
+    generateFx(winFx)
     body.style.background = 'white'
     players[0].winner=true
     holdBtn.removeEventListener('click' , hold)
@@ -117,9 +115,6 @@ function hold (){
   displayTotalScore()
   currentPlayerCss()
 }
-// Hold event 
-holdBtn.addEventListener('click' , hold)
-
 //function affichage current scores 
 function displayCurrentScore() {
   if (currentPlayer = player1) {
@@ -129,43 +124,12 @@ function displayCurrentScore() {
     scores[1].childNodes[3].childNodes[3].textContent = currentPlayer.currentScore
   }
 }
-
-//function affichage total scores 
-function displayTotalScore() {
-  if (currentPlayer = player1) {
-    scores[0].childNodes[1].textContent = currentPlayer.totalScore
-  }
-  if (currentPlayer = player2) {
-    scores[1].childNodes[1].textContent = currentPlayer.totalScore
-  }
-}
-
-// Function currentPlayer Css 
-function currentPlayerCss () {
-  if (players[0]==player1) {
-    body.style.background = 'linear-gradient(90deg, #edf1f1 50%, white 50%)'
-    redDot1.style.opacity = '100'
-    redDot2.style.opacity='0'
-    playerTitre1.style.fontWeight='bolder'
-    playerTitre2.style.fontWeight = 'normal'
-
-  } 
-  if (players[0]==player2) {
-    body.style.background = 'linear-gradient(90deg, white 50%, #edf1f1 50%)'
-    redDot2.style.opacity = '100'
-    redDot1.style.opacity='0'
-    playerTitre2.style.fontWeight='bolder'
-    playerTitre1.style.fontWeight = 'normal'
-  }
-}
-
-// Reset boutton 
+// Reset event 
 newGame.addEventListener('click', newFunction)
 
 // Reset function
 function newFunction() {
-  winFx.pause()
-  newFx.play()
+  generateFx(newFx)
   holdBtn.removeEventListener('click', msg)
   rollBtn.removeEventListener('click', msg)
   holdBtn.addEventListener('click' , hold)
@@ -185,4 +149,46 @@ function newFunction() {
     scores[i].childNodes[1].textContent = players[i].totalScore
   }
 
+}
+
+/*...........................................Functions utlisées.........................................//
+...........................................................................................................*/
+
+//function random
+function rand(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+//function affichage total scores 
+function displayTotalScore() {
+  if (currentPlayer = player1) {
+    scores[0].childNodes[1].textContent = currentPlayer.totalScore
+  }
+  if (currentPlayer = player2) {
+    scores[1].childNodes[1].textContent = currentPlayer.totalScore
+  }
+}
+
+// Function currentPlayer Css 
+function currentPlayerCss() {
+  if (players[0] == player1) {
+    body.style.background = 'linear-gradient(90deg, #edf1f1 50%, white 50%)'
+    redDot1.style.opacity = '100'
+    redDot2.style.opacity = '0'
+    playerTitre1.style.fontWeight = 'bolder'
+    playerTitre2.style.fontWeight = 'normal'
+
+  }
+  if (players[0] == player2) {
+    body.style.background = 'linear-gradient(90deg, white 50%, #edf1f1 50%)'
+    redDot2.style.opacity = '100'
+    redDot1.style.opacity = '0'
+    playerTitre2.style.fontWeight = 'bolder'
+    playerTitre1.style.fontWeight = 'normal'
+  }
+}
+// Function fx sound boutton
+function generateFx(fx) {
+  if (fx.paused) { fx.currentTime = 0; fx.play() }
+  else fx.pause();
 }
