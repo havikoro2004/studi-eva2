d110d98958f0aa50ce304e91747b3ad6bd22f515
flata <havikoro2004@gmail.com>
2021-07-22 09:51:07 +0200

    Nouvelle mise à jour et fix bugs

--- js/script.js
+++ js/script.js
@@ -1,177 +1,161 @@
-// Declaration des vars utilisables pour manipuler le DOM
-
-var body = document.getElementById('body')
-var rollBtn = document.getElementById('roll')
-var holdBtn = document.getElementById('hold')
-var rollHoldDiv = document.getElementById('rollHold')
-var dImg = document.getElementById('dImg')
-var dotIcon1 = document.getElementById('dot-icon')
-var dotIcon2 = document.getElementById('dot-icon2')
-var player1Titre = document.getElementById('p1-text')
-var player2Titre = document.getElementById('p2-text')
-var player1Current = document.getElementById('player1')
-var player2Current = document.getElementById('player2')
-var rollAudio = document.getElementById('roll-fx')
-var loseAudio = document.getElementById('lose-fx')
-var holdAudio = document.getElementById('hold-fx')
-var newAudio = document.getElementById('new-fx')
-var winAudio = document.getElementById('win-fx')
-var newGame =  document.getElementById('new')
-var currentScoreDiv1 = document.getElementById('current1')
-var currentScoreDiv2 = document.getElementById('current2')
-
-
-//Création d'une fonction rand() qui génére un nombre entre 1 et 6 
+//function random
 function rand(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-// Créer la classe Player 
+var body = document.getElementById('body')
+var newGame = document.getElementById('new')
+var rollBtn = document.getElementById('roll')
+var scores = document.getElementsByClassName('scores')
+var holdBtn = document.getElementById('hold')
+var redDot1 = document.getElementById('dot-icon')
+var redDot2 = document.getElementById('dot-icon2')
+var playerTitre1 = document.getElementById('p1-text')
+var playerTitre2 = document.getElementById('p2-text')
 
-class Player {
-  constructor(name, currentScore, totalScore, tour) {
+class Players {
+  constructor(name , currentScore , totalScore , tour){
     this.name = name
     this.currentScore = currentScore
     this.totalScore = totalScore
-    this.tour = tour
+    this.tour = false
+    this.winner = false
   }
 }
 
-// Créer Deux joueurs
-let player1 = new Player('player1', 0, 0, false)
-let player2 = new Player('player2', 0, 0, false)
-let players = [player1, player2]
+var player1 = new Players('player1', 0, 0, false)
+var player2 = new Players('player2', 0, 0, false)
+var players = [player1, player2]
 
+// Commencer la partie 
+let gameTour = 0
+// définir le joueur courant
+let currentPlayer
 
-//Définir début de partie
-var gameTour = 0
+// roll boutton function
 
-// Variable qui présent le joueur courant
-var player;
+function roll() {
 
-
-// function Debounce 
-function debounce(callback, delay) {
-  var timer;
-  return function () {
-    var args = arguments;
-    var context = this;
-    clearTimeout(timer);
-    timer = setTimeout(function () {
-      callback.apply(context, args);
-    }, delay)
-  }
-}
-
-// function ChangeBackgroundColor
-
-function changeBgColor (){
-  if (player1.tour == true) {
-    body.style.background = 'linear-gradient(90deg, #edf1f1 50%, white 50%)'
-    dotIcon2.style.color = 'white'
-    dotIcon1.style.color = 'red'
-    player1Titre.style.fontWeight = 'bolder'
-    player2Titre.style.fontWeight = 'normal'
-
-  } else {
-    body.style.background = 'linear-gradient(90deg, white 50%, #edf1f1 50%)'
-    dotIcon2.style.color = 'red'
-    dotIcon1.style.color = 'white'
-    player2Titre.style.fontWeight = 'bolder'
-    player1Titre.style.fontWeight = 'normal'
-  }
-}
-// Ajouter evenement au click sur le btn Roll
-
-rollBtn.addEventListener('click', debounce(function () {
-
-  rollAudio.play()
-  
-  let nbrRand = rand(1, 7)
+var nbrRand = rand(1,7)
 
   switch (nbrRand) {
-    case 1: dImg.src = 'images/1.jpg';
-    break; 
-    case 2: dImg.src = 'images/2.jpg';
-    break; 
+    case 1: dImg.src = 'images/1.jpg'; 
+      break; 
+    case 2: dImg.src = 'images/2.jpg'; 
+      break;
     case 3: dImg.src = 'images/3.jpg'; 
-    break; case 4: dImg.src = 'images/4.jpg'; 
-    break; case 5: dImg.src = 'images/5.jpg'; 
-    break; case 6: dImg.src = 'images/6.jpg'; 
-    break;
+      break; 
+    case 4: dImg.src = 'images/4.jpg'; 
+      break;
+    case 5: dImg.src = 'images/5.jpg'; 
+      break;
+    case 6: dImg.src = 'images/6.jpg'; 
+      break;
   }
 
-  if (gameTour == 0) {
-    players[0].tour = true
-    player = players[0]
-    player.currentScore += nbrRand
-    var currentScoreArea = document.getElementById(players[0].name).childNodes[3].childNodes[3]
-    
-    if (nbrRand == 1) {
-      setTimeout(() => {
-        dImg.src = 'images/default.jpg'
-      }, 500);
-      rollAudio.pause()
-      loseAudio.pause()
-      loseAudio.play()
-      currentScoreArea.textContent = 0
-      player.currentScore = 0
-      player.tour = false
-      players.reverse()
-      players[0].tour = true
-
-    }
-    currentScoreArea.textContent = players[0].currentScore
+if (gameTour==0) {
+  players[0].tour = true
+  currentPlayer = players[0]
+  currentPlayer.currentScore+=nbrRand
+  displayCurrentScore()
+  currentPlayerCss()
+}
+  
+  if (nbrRand==1) {
+    setTimeout(() => {
+      dImg.src = 'images/default.jpg'
+    }, 500);
+    players[0].currentScore = 0
+    currentPlayer.tour = false
+    players.reverse()
+    players[0].tour=true
+    console.log(players);
+    displayCurrentScore()
   }
 
-  changeBgColor()
 
-}, 400))
+}
 
-// Boutton HOLD 
-holdBtn.addEventListener('click', debounce(function () {
-  holdAudio.play()
-  setTimeout(() => {
-    dImg.src = 'images/default.jpg'
-  }, 500);
-  player=players[0]
-  let total = document.getElementById(players[0].name).childNodes[1]
-  player.totalScore += player.currentScore
-  total.textContent = player.totalScore
-  player.currentScore = 0
-  document.getElementById(players[0].name).childNodes[3].childNodes[3].textContent = player.currentScore
-  player.tour = false
+//Hold fonction
+function hold (){
+  players[0].totalScore+=players[0].currentScore
+  players[0].currentScore=0
+  players[0].tour = false
+  displayCurrentScore()
   players.reverse()
   players[0].tour = true
-  changeBgColor()
+  displayTotalScore()
+  currentPlayerCss()
+}
+// Hold boutton 
+holdBtn.addEventListener('click' , hold)
+rollBtn.addEventListener('click' , roll)
 
-  //winner part
-  if (player.totalScore>=6) {
-      holdAudio.pause()
-      winAudio.play()
-      let winner = player
-      rollHoldDiv.style.display='none'
-      // remove chiledren 
-      document.getElementById(players[1].name).childNodes[3].textContent=''
-      if (winner==player1) {
-        currentScoreDiv1.style.background = 'center / contain no-repeat url("images/win1.jpg")'
-      } else {
-        currentScoreDiv2.style.background = 'center / contain no-repeat url("images/win2.jpg")'
-      }
-      body.style.background = 'linear-gradient(90deg, white 50%, white 50%)'
-      dotIcon2.style.color = 'white'
-      dotIcon1.style.color = 'white'
-      player2Titre.style.fontWeight = 'normal'
-      player1Titre.style.fontWeight = 'normal'
-      return;
+// New Game boutton 
+newGame.addEventListener('click' , newFunction)
+
+// function reset 
+function newFunction() {
+  for (player of players) {
+    player.currentScore = 0
+    player.totalScore = 0
+    player.tour=false
   }
-}, 400))
+  players[0].tour = true
+  currentPlayer = player1
+  currentPlayer = players[0]
+  displayTotalScore()
+  displayCurrentScore()
+  resetCss()
+  roll
+  
+  
+}
+//function affichage current scores 
+function displayCurrentScore() {
+  if (currentPlayer = player1) {
+    scores[0].childNodes[3].childNodes[3].textContent = currentPlayer.currentScore
+  }
+  if (currentPlayer = player2) {
+    scores[1].childNodes[3].childNodes[3].textContent = currentPlayer.currentScore
+  }
+}
 
-// New game
-newGame.addEventListener('click' , ()=>{
-  newAudio.play()
-  setInterval(() => {
-    location.reload() 
-  }, 500);
-})
+//function affichage total scores 
+function displayTotalScore() {
+  if (currentPlayer = player1) {
+    scores[0].childNodes[1].textContent = currentPlayer.totalScore
+  }
+  if (currentPlayer = player2) {
+    scores[1].childNodes[1].textContent = currentPlayer.totalScore
+  }
+}
 
+// Function currentPlayer Css 
+function currentPlayerCss () {
+  if (players[0]==player1) {
+    body.style.background = 'linear-gradient(90deg, #edf1f1 50%, white 50%)'
+    redDot1.style.opacity = '100'
+    redDot2.style.opacity='0'
+    playerTitre1.style.fontWeight='bolder'
+    playerTitre2.style.fontWeight = 'normal'
+
+  } 
+  if (players[0]==player2) {
+    body.style.background = 'linear-gradient(90deg, white 50%, #edf1f1 50%)'
+    redDot2.style.opacity = '100'
+    redDot1.style.opacity='0'
+    playerTitre2.style.fontWeight='bolder'
+    playerTitre1.style.fontWeight = 'normal'
+  }
+  
+}
+
+// reset Css
+function resetCss() {
+  body.style.background = 'white'
+  redDot2.style.opacity = '0'
+  redDot1.style.opacity = '0'
+  playerTitre2.style.fontWeight = 'normal'
+  playerTitre1.style.fontWeight = 'normal'
+}
